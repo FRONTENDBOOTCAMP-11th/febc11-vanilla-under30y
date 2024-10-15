743ed6406c5d105533bf98f23405bf323d290a5e
underthemo-onlight <underthemo_onlight@naver.com>
2024-10-15 13:49:11 +0900

    fix

---
+++ sleep.js
@@ -0,0 +1,16 @@
+// Sleep 함수 정의 (ms 단위로 지연)
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  // 사용 예시
+  async function demo() {
+    console.log("Start");
+    
+    // 2초 지연
+    await sleep(2000);
+    
+    console.log("End after 2 seconds");
+  }
+  
+  demo();
