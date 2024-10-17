ad3048325ebf77741113b83d6780059e0cf48a67
guswhdleee <gksrmrtn123@naver.com>
2024-10-17 10:22:03 +0900

    feat : 제품 리스트 html 및 css 작성

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        index: resolve(__dirname, 'index.html'), // 기본 index.html
-        // login: resolve(__dirname, 'src/pages/auth/login.html'), // 추가 HTML 파일
-        // list: resolve(__dirname, 'src/pages/board/list.html'), // 추가 HTML 파일
-        // 필요한 다른 HTML 파일을 여기에 추가
-      },
-    },
-  },
-});
+	build: {
+		rollupOptions: {
+			input: {
+				index: 'index.html', // 기본 index.html
+				login: 'src/pages/auth/login.html', // 추가 HTML 파일
+				list: 'src/pages/board/list.html' // 추가 HTML 파일
+				// 필요한 다른 HTML 파일을 여기에 추가
+			}
+		}
+	}
+})
