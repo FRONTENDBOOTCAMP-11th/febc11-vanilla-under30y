632a3cca39426015d3c8f872bfeb3ade4c3199c2
underthemo-onlight <underthemo_onlight@naver.com>
2024-10-21 17:57:54 +0900

    헤더 푸터 템플릿 적용

---
+++ src/main/test.js
@@ -0,0 +1,23 @@
+class NikeHeader extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    const template = document.getElementById("nike-header");
+    const clone = template.content.cloneNode(true);
+    this.shadowRoot.appendChild(clone);
+  }
+}
+
+customElements.define("nike-header", NikeHeader);
+
+class NikeFooter extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    const template = document.getElementById("nike-footer");
+    const clone = template.content.cloneNode(true);
+    this.shadowRoot.appendChild(clone);
+  }
+}
+
+customElements.define("nike-footer", NikeFooter);
