be4301a533299d4e17d215e757c5b5534108d6cc
underthemo-onlight <underthemo_onlight@naver.com>
2024-10-23 09:50:10 +0900

    배포를 위한 파일 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: {
-				index: 'index.html', // 기본 index.html
-				login: 'src/pages/auth/login.html', // 추가 HTML 파일
-				list: 'src/pages/board/list.html' // 추가 HTML 파일
+				index: 'src/main/main.html', // 기본 index.html 
+				login: 'src/login/login.html', // 추가 HTML 파일
+				signup: 'src/signup/singup.html', // 추가 HTML 파일
+				product_detail: 'src/productDetail/productDetail.html', // 추가 HTML 파일
+				item_list: 'src/ItemList/itemList.html',
+				cart: 'src/cart/prdBasket.html'
 				// 필요한 다른 HTML 파일을 여기에 추가
 			}
 		}
