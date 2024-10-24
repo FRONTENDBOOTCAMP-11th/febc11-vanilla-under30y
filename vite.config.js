5993fd1799af9023f54862f0dda0509ee0cf8257
Taiho1998 <tgo12323@gmail.com>
2024-10-24 09:26:37 +0900

    fix/배포 테스트를 위해 파일 위치 및 오타 수정

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: {
-				index: 'src/main/main.html', // 기본 index.html 
+				index: 'index.html', // 기본 index.html
 				login: 'src/login/login.html', // 추가 HTML 파일
-				signup: 'src/signup/singup.html', // 추가 HTML 파일
+				signup: 'src/signup/signup.html', // 추가 HTML 파일
 				product_detail: 'src/productDetail/productDetail.html', // 추가 HTML 파일
 				item_list: 'src/ItemList/itemList.html',
 				cart: 'src/cart/prdBasket.html'
