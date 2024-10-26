480607f4bf4b7ad5a294cce53c160a07602d6e9e
Taiho1998 <tgo12323@gmail.com>
2024-10-27 00:41:12 +0900

    fix/header와 footer template 변경, summary 애니메이션 적용 방식 변경

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,8 @@ import pluginJs from '@eslint/js'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    { rules: { 'no-unused-vars': 'warn' } },
-    eslintConfigPrettier
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	{ rules: { 'no-unused-vars': 'warn' } },
+	eslintConfigPrettier
 ]
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                index: 'index.html', // 기본 index.html
-                login: 'src/login/login.html', // 추가 HTML 파일
-                signup: 'src/signup/signup.html', // 추가 HTML 파일
-                product_detail: 'src/productDetail/productDetail.html', // 추가 HTML 파일
-                item_list: 'src/ItemList/itemList.html',
-                cart: 'src/cart/prdBasket.html'
-                // 필요한 다른 HTML 파일을 여기에 추가
-            }
-        }
-    }
+	build: {
+		rollupOptions: {
+			input: {
+				index: 'index.html', // 기본 index.html
+				login: 'src/login/login.html', // 추가 HTML 파일
+				signup: 'src/signup/signup.html', // 추가 HTML 파일
+				product_detail: 'src/productDetail/productDetail.html', // 추가 HTML 파일
+				item_list: 'src/ItemList/itemList.html',
+				cart: 'src/cart/prdBasket.html'
+				// 필요한 다른 HTML 파일을 여기에 추가
+			}
+		}
+	}
 })
